074a28a486a32b1eaecad130717a1fc29ccef7f4
Andrew B <brei0017@gmail.com>
2018-11-08 17:24:01 -0800

    Upload Car Insurance Boolean assignment from step 75 of C# course.

---
+++ C-Sharp-Coding-Projects/CarInsuranceBoolean/CarInsuranceBoolean/CarInsuranceBoolean.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarInsuranceBoolean
+{
+    class CarInsuranceBoolean
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?  Enter \"true\" or \"false\": ");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets have you had?");
+            int speedingTickets = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = age > 15 && dui == false && speedingTickets <= 3;
+            Console.WriteLine(qualified);
+            Console.Read();
+
+
+        }
+    }
+}
