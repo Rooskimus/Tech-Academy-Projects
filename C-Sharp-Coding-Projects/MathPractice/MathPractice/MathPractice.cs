26c18a07145017506f5c651b55a529089ce5070a
Andrew B <brei0017@gmail.com>
2018-11-08 16:12:27 -0800

    Upload Math Practice assignment for step 66 of C# course.

---
+++ C-Sharp-Coding-Projects/MathPractice/MathPractice/MathPractice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPractice
+{
+    class MathPractice
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter any number: ");
+            double userNumber = Convert.ToDouble(Console.ReadLine());
+            double timesFifty = userNumber * 50.0;
+            Console.WriteLine("Your number times 50: " + timesFifty);
+            double plusTwentyFive = userNumber + 25.0;
+            Console.WriteLine("Your number plus 25: " + plusTwentyFive);
+            double overTwelvePointFive = userNumber / 12.5;
+            Console.WriteLine("Your number divided by 12.5: " + overTwelvePointFive);
+            bool greaterFifty = userNumber > 50;
+            Console.WriteLine("Your number is over 50: " + greaterFifty);
+            double remainder = userNumber % 7.0;
+            Console.WriteLine("The remainder of " + userNumber + " divided by 7: " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
