434e3c08efaa2c6e740efde06dee966c4a389249
Andrew B <brei0017@gmail.com>
2018-11-09 17:14:39 -0800

    Upload String Drill and String Notes per step 97 of C# course.

---
+++ C-Sharp-Coding-Projects/AboutStrings/AboutStrings/AboutStrings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace AboutStrings
+{
+    class AboutStrings
+    {
+        static void Main()
+        {
+            Console.WriteLine("The man said \"Hello.\""); // Backslash is an "escape character" which allows you to "escape" the standard formatting of a string.
+            Console.WriteLine("This will have \na line break."); // \n is the line break.  Notice the backslash affects only characters immediately after it.
+            Console.WriteLine("\tThis line has been tabbed."); // \t is for tab.
+            Console.WriteLine("To display \\ you have to type \"\\\\\""); // \\ is the escape character followed by a backsplash you'd like to display.
+            Console.WriteLine(@"Everything\inside\should\display\including\backslashes\"); // the @ in front of quotation marks allows \ to not be the escape character.
+            Console.WriteLine("But using the @ symbol as in the previous line you cannot display a \\ and a \" because the backslash doesn't count as an escape character.");
+
+            // Some convenient ways to manipulate or use strings:
+            string name = "Andrew";
+            Console.WriteLine(name);
+            bool trueOrFalse = name.Contains("s");
+            bool endsWithW = name.EndsWith("w");
+            Console.WriteLine("Does " + name + " contain an S? " + trueOrFalse);
+            Console.WriteLine("Does " + name + " end with a W? " + endsWithW);
+
+            int length = name.Length;
+            Console.WriteLine(name + " has " + length + " characters.");
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+
+            //  Strings are immutable, which means that every time you change a string, you're actually simply creating a new string.
+            //  The issue is this will leave the old string in your memory.  Not a big deal on small projects, but when strings that 
+            //  are being modified number in the thousands, you start to run very low on memory and things slow to a crawl.
+            //  Enter StringBuilder, a dynamic object created to have expandable strings while using the same memory.
+
+            StringBuilder myName = new StringBuilder();
+
+            myName.Append("My name is Andrew");
+            Console.WriteLine(myName);
+            myName.Append(" and I'm testing appending.");
+            Console.WriteLine(myName);
+            myName.Remove(18, 26); // The 18 says where to start removing, the 26 says how many characters to remove.
+            Console.WriteLine(myName);
+
+            Console.Read();
+        }
+    }
+}
---
+++ C-Sharp-Coding-Projects/StringDrill/StringDrill/MadLib.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDrill
+{
+    class MadLib
+    {
+        static void Main()
+        {
+            Console.WriteLine("Let's do some Mad Libs!");
+            Console.WriteLine("Enter an adjective: ");
+            string adjective = Console.ReadLine();
+            Console.WriteLine("Enter an adverb: ");
+            string adverb = Console.ReadLine();
+            Console.WriteLine("Enter a singular noun: ");
+            string noun = Console.ReadLine();
+            Console.WriteLine("Enter a plural noun: ");
+            string nounTwo = Console.ReadLine();
+            Console.WriteLine("\nOnce there was a " + adjective + " " + noun + " which liked to " + adverb + " eat " + nounTwo + ".");
+            Console.ReadLine();
+            StringBuilder pgrph = new StringBuilder();
+            pgrph.Append("But one day the " + nounTwo + " got the better of the " + adjective + " " + noun + ".");
+            pgrph.Append("  They did this by " + adverb + " eating themselves instead.");
+            pgrph.Append("  This confused the " + adjective + " " + noun + " to no end.");
+            pgrph.Append("  In fact, the " + adjective + " " + noun + " became so confused it " + adverb.ToUpper() + " ate itself!");
+            Console.WriteLine(pgrph);
+            Console.Read();
+        }
+    }
+}
