52e92f0018512f4e17a9d2717864a955a54a33c2
Andrew B <brei0017@gmail.com>
2018-11-10 12:57:43 -0800

    Upload Array and List Drills and Tutorial with notes.

---
+++ C-Sharp-Coding-Projects/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Arrays: used for things with FIXED quantities as the length is always SET.
+            int[] numArray = new int[5]; // Declares an array with the name numArray containing integers with a length of 5.
+            numArray[0] = 5;
+            numArray[1] = 2;
+            numArray[2] = 10;
+            numArray[3] = 200;
+            numArray[4] = 5000;
+
+            int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 }; // Shorthand way to assign values.  
+            // You may enter any number of values without setting the number beforehand.
+            // However, once the length is set it is fixed!
+
+            int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 }; // Even shorter shorthand.
+
+            numArray2[5] = 650;
+
+            Console.WriteLine(numArray2[5] + "\n");
+
+
+            // Lists: used for things with variable quantities as length is flued; arrays can be better for large amounts of data though.
+            List<int> intList = new List<int>(); // Declares and instantiates a list named intList.
+            intList.Add(4);  // adds 4 to index 0
+            intList.Add(10); // adds 10 to index 1
+            intList.Remove(4); // removes 4 from index 0 and automatically reduces size of list so that 10 is now index 0.
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C-Sharp-Coding-Projects/ListAndArraysDrill/ListAndArraysDrill/MisfortuneTeller.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ListAndArraysDrill
+{
+    class MisfortuneTeller
+    {
+        static void Main()
+        {
+            string[] unpleasantThings = { "Stingy shots.", "Mosquitos.",
+                "Mint flavored jelly", "Running out of soap while in the shower.",
+                "That feeling that you've forgotten something but you can't remember exactly what it was." };
+            Console.Write("Enter a number to see which unpleasant thing awaits you: ");
+            string inputUnpleasant = Console.ReadLine();
+            int inputUnpleasantCheck = EnterValidNumber(inputUnpleasant);
+            inputUnpleasantCheck = EnterValidIndex(inputUnpleasantCheck, unpleasantThings);
+            Console.WriteLine("The unpleasant thing that will visit upon you will be: " + unpleasantThings[inputUnpleasantCheck]);
+            Console.ReadLine();
+
+            int[] unluckyNumbers = { 21, 13, 72, 3, 8, 2, 1347 };
+            Console.Write("Enter a number to see what your unlucky number will be today: ");
+            string inputUnlucky = Console.ReadLine();
+            int unluckyCheck = EnterValidNumber(inputUnlucky);
+            string[] unluckyNumbersString = unluckyNumbers.Select(x => x.ToString()).ToArray(); // This creates a string array from my int array so it may be used by EnterValidIndex which only accepts string arrays.
+            unluckyCheck = EnterValidIndex(unluckyCheck, unluckyNumbersString);
+            Console.WriteLine("The number you had best avoid today is: " + unluckyNumbers[unluckyCheck]);
+            Console.ReadLine();
+
+            List<string> chokeFood = new List<string>();
+            string[] foods = { "Bananas." , "Hot Dogs.", "Cucumbers sliced into circles.  Stick-shaped is fine.",
+                "Anything spiced with basil." , "Medium-sized oranges.", "Rudabega." };
+            chokeFood.AddRange(foods);
+            Console.WriteLine("Enter a number and see on which food you will surely choke and die if you consume it today: ");
+            string inputFood = Console.ReadLine();
+            int inputFoodCheck = EnterValidNumber(inputFood);
+            inputFoodCheck = EnterValidIndexList(inputFoodCheck, chokeFood);
+            Console.WriteLine("The food that will surely choke you today if you choose to eat it is: " + chokeFood[inputFoodCheck]);
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("\n>> Heed these warnings and have a good day! <<");
+            Console.ReadLine();
+
+
+
+        }
+
+        static int EnterValidNumber(string x) // This ensures that only integers are passed into the array to seek an index.
+        {
+            int y;
+            while (!Int32.TryParse(x, out y))
+            {
+                Console.WriteLine("Please enter numbers only: ");
+                x = Console.ReadLine();
+            }
+            return y;
+        }
+
+        static int EnterValidIndex(int x, string[] z) // This ensures the user will know what options are available if they do not choose a valid one and prevent errors.
+        {
+            int zlength = z.Length;
+            while (x >= zlength)
+            {
+                Console.WriteLine("There are only " + zlength + " options, please enter a 0 - " + (zlength - 1)); // I made this relative to whatever string is passed for usability.
+                string input = Console.ReadLine();
+                x = EnterValidNumber(input);
+            }
+            return x;
+        }
+
+        static int EnterValidIndexList(int x, List<string> z)  // Modified the above subroutine for Lists instead of arrays.
+        {
+            int zlength = z.Count;
+            while (x >= zlength)
+            {
+                Console.WriteLine("There are only " + zlength + " options, please enter a 0 - " + (zlength - 1));
+                string input = Console.ReadLine();
+                x = EnterValidNumber(input);
+            }
+            return x;
+        }
+    }
+}
