6a61fb80e3295c562eea3fb8c93c860f6077b94e
Andrew B <brei0017@gmail.com>
2018-11-12 12:25:24 -0800

    Upload Exception Handling Drill and Tutorial

---
+++ C-Sharp-Coding-Projects/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try // Anything that will be checked for errors goes here.
+            {
+                Console.WriteLine("Pick a number: ");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another number: ");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " is " + numberThree);
+                Console.Read();
+            }
+            catch (FormatException ex) // Specific errors may be caught and dealt with individually
+            {
+                Console.WriteLine("Please type a whole number.");
+                return; // used to break out of the program.
+            }
+            catch (DivideByZeroException ex) // Another specific error.
+            {
+                Console.WriteLine("You can't divide by zero, silly goose.");
+                return;
+            }
+            catch (Exception ex) // This will catch any unforseen errors
+            {
+                Console.WriteLine(ex.Message); // This simply returns the default error message for any error.
+                return;
+            }
+            finally // This runs before the program ends regardless of what's in the catches.  Without this, we wouldn't see the error
+                    // messages from above as the program would just end at the return.
+            {
+                Console.Read();
+            }
+
+        }
+    }
+}
---
+++ C-Sharp-Coding-Projects/ExceptionHandlingDrill/ExceptionHandlingDrill/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numberList = new List<int>() { 1, 22, 124, 550, 2000, 4 };
+                Console.WriteLine("Beep boop. Remote integer list accepted. Boop beep.  User, please enter denominator: ");
+                int denominator = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Buzz cluck beep.  Denominator accepted.  Initiating turing process...");
+                foreach (int number in numberList)
+                {
+                    int result = number / denominator;
+                    Console.WriteLine(result);
+                }
+                Console.WriteLine("\nTabulation complete.  Terminating turing process.");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Zero is an unnaceptable devisor.  Are you trying to overload my circuits?  Enter a non-zero number, human.");
+                return;
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Input is prohibited data type.  Only integers accepted.");
+                return;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            finally
+            {
+                Console.WriteLine("Exiting try/catch block.  Terminating algorithm.  Enjoy your day.");
+                Console.Read();
+            }
+
+
+        }
+    }
+}
