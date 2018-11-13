04fea7ec6629e6d74bc0182c197c1ec86ae7da28
Andrew B <brei0017@gmail.com>
2018-11-12 17:22:40 -0800

    Upload Class and Method Drill 4 for step 115 of C# course.

---
+++ C-Sharp-Coding-Projects/ClassAndMethodDrill4/ClassAndMethodDrill4/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodDrill4
+{
+    class MyClass
+    {
+        public int MyMethod(int x, int y = 0)
+        {
+            int z = (x * x) + (y * y);
+            return z;
+        }
+    }
+}
---
+++ C-Sharp-Coding-Projects/ClassAndMethodDrill4/ClassAndMethodDrill4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodDrill4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            int output = 0;
+            Console.WriteLine("Let's perform a math operation on two integers.");
+            Console.Write("Please choose your first integer: ");
+            string inOne = Console.ReadLine();
+            bool isIntOne = int.TryParse(inOne, out int inputOne);
+            while (!isIntOne)
+            {
+                Console.WriteLine("Please enter a valid integer: ");
+                inOne = Console.ReadLine();
+                isIntOne = int.TryParse(inOne, out inputOne);
+            }
+
+
+            Console.Write("OK, and now your second.  This one is optional, you may simply press enter if you please: ");
+            string inTwo = Console.ReadLine();
+            bool isIntTwo = int.TryParse(inTwo, out int inputTwo);
+            if (isIntTwo)
+            {
+                output = myClass.MyMethod(inputOne, inputTwo);
+            }
+            else
+            {
+                output = myClass.MyMethod(inputOne);
+            }
+            Console.WriteLine("Your result is: {0}", output);
+            Console.Read();
+        }
+    }
+}
