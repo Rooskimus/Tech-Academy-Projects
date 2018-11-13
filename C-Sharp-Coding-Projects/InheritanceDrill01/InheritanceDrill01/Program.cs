0e88ceefba1b0ac5b488ffaf4e3f01fca0cbf052
Andrew B <brei0017@gmail.com>
2018-11-13 14:39:39 -0800

    Upload Inheritance Drill 01 for step 120 of C# course.

---
+++ C-Sharp-Coding-Projects/InheritanceDrill01/InheritanceDrill01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDrill01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Sample = new Employee() { Id = 001, FirstName = "Sample", LastName = "Student" };
+            Sample.SayName();
+            Console.ReadLine();
+        }
+    }
+}
