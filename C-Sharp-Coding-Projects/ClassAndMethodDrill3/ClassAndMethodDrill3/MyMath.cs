fc0dfa749a88e8ce77ee8b4b78274a1e0248efe0
Andrew B <brei0017@gmail.com>
2018-11-12 16:59:59 -0800

    Upload Class and Method Drill 3 for step 114 of C# course.

---
+++ C-Sharp-Coding-Projects/ClassAndMethodDrill3/ClassAndMethodDrill3/MyMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodDrill3
+{
+    class MyMath
+    {
+        public int MyMethodOne(int x)
+        {
+            x = x * x;
+            return x;
+        }
+
+        public int MyMethodOne(decimal x)
+        {
+            x = x + 21;
+            return (int)x;
+        }
+
+        public int MyMethodOne(string x)
+        {
+            int y = int.Parse(x);
+            y = y * 2;
+            return y;
+        }
+
+    }
+}
---
+++ C-Sharp-Coding-Projects/ClassAndMethodDrill3/ClassAndMethodDrill3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath numbers = new MyMath();
+            int resultOne = numbers.MyMethodOne(33);
+            int resultTwo = numbers.MyMethodOne(9.23402100m);
+            int resultThree = numbers.MyMethodOne("7");
+
+            Console.WriteLine("Result 1: {0}\nResult 2: {1}\nResult 3: {2}", resultOne, resultTwo, resultThree);
+            Console.Read();
+        }
+    }
+}
