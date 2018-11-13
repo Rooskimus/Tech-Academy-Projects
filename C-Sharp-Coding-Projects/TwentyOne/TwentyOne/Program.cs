d952b958a3f5eb2e29a5b5bbd538dc0ad745d6d6
Andrew B <brei0017@gmail.com>
2018-11-12 17:23:13 -0800

    Initialize TwentyOne in GitHub.

---
+++ C-Sharp-Coding-Projects/TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Deck deck = new Deck();
+            int timesShuffled = 0;
+            Shuffle(deck, out timesShuffled, 5); // the deck: and times: are named parameters just to make the code easier to read.
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }          
+            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times shuffled: {0}", timesShuffled);  // using a bracket with a number {0} {1} {2}, variable0, variable1, variable2 allows you to easily assign variables in a string.
+            Console.Read();
+        }
+
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1) // giving int times a default value of 1 makes it an optional parameter.
+        {
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
+            {
+                timesShuffled++;
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = TempList;
+            }
+            return deck;
+        }
+
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
+    }
+}
---
+++ C-Sharp-Coding-Projects/TwentyOne/TwentyOne/card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card // "public" makes the class accessible to other parts of the program.
+    {
+        public Card() // this is a constructor
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }  // Declares that the Card class has a property named Suit. You can do two things with the Suite, get or set.
+        public string Face { get; set; }
+        // classes are just a design for an object, we don't actually set the Face or the Suit here!
+    }
+}
