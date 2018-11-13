d77b5e7fb9808f0c07fc8a6058e264a3ac406da9
Andrew B <brei0017@gmail.com>
2018-11-12 16:39:48 -0800

    Upload Class and Method Drill 2 for step 113 of C# course.

---
+++ C-Sharp-Coding-Projects/ClassAndMethodDrill2/ClassAndMethodDrill2/Demo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodDrill2
+{
+    class Demo
+    {
+        public void DemoMethod(int x, int y)
+        {
+            x = x * x;
+            Console.WriteLine("Displaying parameter y: {0}", y);
+        }
+    }
+}
---
+++ C-Sharp-Coding-Projects/ClassAndMethodDrill2/ClassAndMethodDrill2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo demonstrate = new Demo();
+            demonstrate.DemoMethod(3, 5);
+            demonstrate.DemoMethod(x: 4, y: 6);
+            Console.Read();
+        }
+    }
+}
