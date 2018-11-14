406399ffb3f356c2b7bffb09845d0e4c3c6d846b
Andrew B <brei0017@gmail.com>
2018-11-14 12:21:49 -0800

    Modify Employee class with overloaded operators for the drill on step 129 of C# course.

--- C-Sharp-Coding-Projects/InheritanceDrill01/InheritanceDrill01/Employee.cs
+++ C-Sharp-Coding-Projects/InheritanceDrill01/InheritanceDrill01/Employee.cs
@@ -9,5 +9,27 @@ namespace InheritanceDrill01
     public class Employee : Person
     {
         public int Id { get; set; }
+
+        public static bool operator == (Employee employeeOne, Employee employeeTwo)
+        {
+            return employeeOne.Id == employeeTwo.Id;
+        }
+
+        public static bool operator != (Employee employeeOne, Employee employeeTwo)
+        {
+            return !(employeeOne == employeeTwo); // uses the above == operator
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
     }
 }
--- C-Sharp-Coding-Projects/InheritanceDrill01/InheritanceDrill01/Program.cs
+++ C-Sharp-Coding-Projects/InheritanceDrill01/InheritanceDrill01/Program.cs
@@ -11,8 +11,18 @@ namespace InheritanceDrill01
         static void Main(string[] args)
         {
             Employee Sample = new Employee() { Id = 001, FirstName = "Sample", LastName = "Student" };
+            Employee SampleTwo = new Employee() { Id = 001, FirstName = "Test", LastName = "Employee" };
+            Employee SampleThree = new Employee() { Id = 002, FirstName = "Unique", LastName = "Employee" };
             Sample.SayName();
+            SampleTwo.SayName();
+            SampleThree.SayName();
+            bool checkOne = Sample == SampleTwo;
+            bool checkTwo = Sample == SampleThree;
+            Console.WriteLine("Do {0} {1} and {2} {3} have the same ID number?: {4}", Sample.FirstName, Sample.LastName, SampleTwo.FirstName, SampleTwo.LastName, checkOne);
+            Console.WriteLine("Do {0} {1} and {2} {3} have the same ID number?: {4}", Sample.FirstName, Sample.LastName, SampleThree.FirstName, SampleThree.LastName, checkTwo);
             Console.ReadLine();
         }
+
+        
     }
 }
