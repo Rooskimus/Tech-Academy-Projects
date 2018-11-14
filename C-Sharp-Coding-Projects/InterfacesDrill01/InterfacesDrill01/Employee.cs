954d3c5d41cc1c31cffd58677870ee878030a752
Andrew B <brei0017@gmail.com>
2018-11-13 17:26:35 -0800

    Upload Interface Drill 01 for step 126 of C# course.

---
+++ C-Sharp-Coding-Projects/InterfacesDrill01/InterfacesDrill01/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDrill01
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("{0} {1}", FirstName, LastName);
+            Console.ReadLine();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("My name is {0} {1}.  Why are you laughing?  You guys are so immature!  I quit!", FirstName, LastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C-Sharp-Coding-Projects/InterfacesDrill01/InterfacesDrill01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDrill01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quitter = new Employee() { FirstName = "Wayne", LastName = "Carr" };
+            quitter.Quit();
+        }
+    }
+}
