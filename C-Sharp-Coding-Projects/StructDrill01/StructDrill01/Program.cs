6262dc3a2156f5fac64d617692278e93b89de6ab
Andrew B <brei0017@gmail.com>
2018-11-15 14:52:05 -0800

    Upload Struct Drill 01 for step 137 of C# course.

---
+++ C-Sharp-Coding-Projects/StructDrill01/StructDrill01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrill01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number() { Amount = 100.7m };
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
