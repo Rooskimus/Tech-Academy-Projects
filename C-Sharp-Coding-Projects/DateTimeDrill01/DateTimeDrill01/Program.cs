17f2cb4da3be0ea2568851e47d41f0eea0730439
Andrew B <brei0017@gmail.com>
2018-11-19 15:59:55 -0800

    Upload Date Time Drill 01 per step 156 of C# course.

---
+++ C-Sharp-Coding-Projects/DateTimeDrill01/DateTimeDrill01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is currently " + DateTime.Now + ".\nEnter a number: ");
+            string numberIn = Console.ReadLine();
+            bool isDouble = Double.TryParse(numberIn, out double number);
+            while (!isDouble)
+            {
+                Console.WriteLine("Is {0} a number written with digits?  I don't think so.  You should know better.  Shame on you.  \n*sigh* Try again: ", numberIn);
+                numberIn = Console.ReadLine();
+                isDouble = Double.TryParse(numberIn, out number);
+            }
+            TimeSpan hoursFuture = TimeSpan.FromHours(number);
+            DateTime result = DateTime.Now + hoursFuture;
+            Console.WriteLine("\n{0} hours in the future the exact date and time will be {1}.", number, result);
+            Console.Read();
+        }
+    }
+}
