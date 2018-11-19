116648369b1191dcbcd27380b1507a5e5aece934
Andrew B <brei0017@gmail.com>
2018-11-19 15:18:55 -0800

    Upload Log Text File Drill for step 154 of C# program.

---
+++ C-Sharp-Coding-Projects/LogTextFileDrill/LogTextFileDrill/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace LogTextFileDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number to log to the file: ");
+            string number = Console.ReadLine();
+            string strPath = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory); // This gets the desktop directory of the machine running the program.
+            File.WriteAllText(strPath + "\\log.txt", number); // I'm appending the name of the file to create on the desktop.
+            string readNumber = File.ReadAllText(strPath + "\\log.txt"); // Then reading that same file.
+            Console.WriteLine("The number you logged as is being read from the log created is: " + readNumber);
+            Console.ReadLine();
+        }
+    }
+}
