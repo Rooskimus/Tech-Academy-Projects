b6df82e8007469f7d4985dbc0a146e9f0bac1154
Andrew B <brei0017@gmail.com>
2018-11-20 12:01:53 -0800

    Upload Extra Features Drill 01 for step 162 of C# course.

---
+++ C-Sharp-Coding-Projects/ExtraFeaturesDrill01/ExtraFeaturesDrill01/Clone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraFeaturesDrill01
+{
+    class Clone
+    {
+        public Clone (string designation, int dnaMatched, int indivIndex)
+        {
+            this.CloneDesignation = designation;
+            this.PercentDNAMatched = dnaMatched;
+            this.IndividualityIndex = indivIndex;
+        }
+        public Clone(string designation) : this(designation, 100, 0) { }
+
+        public int PercentDNAMatched { get; set; }
+        public string CloneDesignation { get; set; }
+        public int IndividualityIndex { get; set; }
+    }
+}
---
+++ C-Sharp-Coding-Projects/ExtraFeaturesDrill01/ExtraFeaturesDrill01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraFeaturesDrill01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string dnaSource = "Steve Jobs";
+            var CloneList = new List<Clone>();
+            Console.WriteLine("We must have more {0}es!  How many clones should we create?", dnaSource);
+            var numberClones = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Very well, begin the cloning process for {0} {1}es!", numberClones, dnaSource);
+            for (int i = 0; i < numberClones; i++)
+            {
+                var designation = String.Format("Steve Jobsoid {0}", i);
+                var newClone = new Clone(designation);
+                CloneList.Add(newClone);
+            }
+            var cloneTotal = CloneList.Count();
+            Console.WriteLine("Success!  We have created {0} {1}es!  Now we will have all the i-Things!", cloneTotal, dnaSource);
+            Console.ReadLine();
+        }
+    }
+}
